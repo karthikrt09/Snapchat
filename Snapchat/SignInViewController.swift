2e1a007f705436bb797419564b2f8bc675e99c68
karthikrt09 <karthikrt.rt@gmail.com>
2017-09-26 23:57:17 -0700

    added code to Sign In View controller and installed some extra pod files

--- Snapchat/SignInViewController.swift
+++ Snapchat/SignInViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var passwordTextFiled: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     
     @IBAction func turnUpButtonTapped(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextFiled.text!, completion: { (user, error) in
+            
+            print("We tried to signin")
+            
+            if error !=nil {
+                print("There is an error:\(error)")
+            } else {
+                print("Signed in Successfully")
+            }
+            
+        })
     }
-
+    
 }
 
