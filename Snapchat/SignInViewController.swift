7f33951668745c9556d9aa40279d43553147f0c2
karthikrt09 <karthikrt.rt@gmail.com>
2017-09-26 23:05:07 -0700

    Firebase Connected, Sign In page done

---
+++ Snapchat/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignInViewController.swift
+//  Snapchat
+//
+//  Created by Karthik Thirunavukkarasan on 9/26/17.
+//  Copyright © 2017 Karthik Thiru. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextFiled: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func turnUpButtonTapped(_ sender: Any) {
+    }
+
+}
+
